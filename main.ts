d9386829ac96ab992c282fa7431ca6b66ab280fa
Marko Anic <marko.anic@htecgroup.com>
2018-01-25 10:20:18 +0100

    Completed the Getting Started with Reactive Programming Using RxJS course

--- main.ts
+++ main.ts
@@ -1,22 +1,51 @@
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import {load, loadWithFetch} from './loader';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
 
-let circle = document.getElementById('circle');
-let source = Observable.fromEvent(document, "mousemove")
-    .map((e : MouseEvent) => { 
-        return {
-            x: e.clientX,
-            y: e.clientY,
-        }
-    }).filter(value => value.x < 500)
-    .delay(300);
+// let source = Observable.merge(
+//     Observable.of(1),
+//     Observable.from([2, 3, 4]),
+//     Observable.throw(new Error('stop!')),
+//     Observable.of(5)
+// ).catch(e => {
+//     console.log(`caught: ${e}`);
+//     return Observable.of(10);
+// });
 
-function onNext(value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+// source.subscribe(
+//     value => console.log(`value: ${value}`),
+//     error => console.log(`error: ${error}`),
+//     () => console.log('complete'),
+// );
+
+let output = document.getElementById('output');
+let button = document.getElementById('button');
+
+let click = Observable.fromEvent(button, "click");
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
 }
 
-source.subscribe(
-    onNext,
-    e => console.log(`error: ${e}`),
-    () => console.log('complete')   
-);
+let subscription =
+    load('moviess.json')
+        .subscribe(
+            renderMovies,
+            e => console.log(`error: ${e}`),
+            () => console.log('complete!')
+        );
+
+console.log(subscription);
+subscription.unsubscribe();
+
+click.flatMap(e => loadWithFetch('movies.json'))
+    .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log('complete!')
+    );
+
